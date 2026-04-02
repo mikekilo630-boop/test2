b112cfda6abc357498bbb37c5eafa77aaa761a6b
mikekilo630-boop <mikekilo630@gmail.com>
2026-04-02 11:35:29 +0200

    Add files via upload

---
+++ logicalerr.c
@@ -0,0 +1,14 @@
+// C program to demonstrate
+// a logical error
+#include <stdio.h>
+
+// Driver code
+int main() 
+{
+int i; 
+for(i = 0; i <= 5; i++); 
+{
+	printf("Geeks for Geeks");
+}
+	return 0;
+}
